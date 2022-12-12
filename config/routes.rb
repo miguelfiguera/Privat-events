2af932f0c78eaafceafb165b6ef3e65fef4016d2
Miguel Figuera <miguelqui725@gmail.com>
2022-12-12 17:50:42 -0400

    log02, user model and events model created, users needs name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :events
+
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "events#index"
 end
