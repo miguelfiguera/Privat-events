1f4b22097c418d15f287ab43b211a20f8677e6c8
Miguel Figuera <miguelqui725@gmail.com>
2022-12-12 17:56:42 -0400

    log03, working on asociations between users and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+    has_many :guests, class_name: 'User'
+    belongs_to :host, class_name: 'user'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :hosted_parties, class_name: 'events'
+  has_many :invited_parties, class_name: 'events'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
