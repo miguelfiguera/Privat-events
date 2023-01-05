4553e0a3522522c0e920ab362992bbe616462b9e
Miguel Figuera <miguelqui725@gmail.com>
2023-01-05 17:08:45 -0400

    log06, creating partials for users show for the events

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -8,9 +8,38 @@ class InvitationsController < ApplicationController
         @invitation=Invitation.find(params)
     end
 
+    def new
+        @invitation=Invitation.new 
+    end
+
+    def create
+        @invitation=current_user.invitations.build(invitation_params)
+
+       
+        if @invitation.save
+            redirect_to root_path
+          else
+            render :new
+          end 
+    end
+
+    def edit
+        @invitation=Invitation.find(current_user.id)
+    end
+
+    def update
+        @invitation=Invitation.find(params[:id])
+
+        if @invitation.update(invitation_params)
+            redirect_to root_path
+        else
+            redirect_to root_path
+        end
+    end
+
 
     private
-    def params
-        params.require(:invitation).permit(:user_id,:event_id)
+    def invitation_params
+        params.require(:invitation).permit(:user_id,:event_id,:acceptance)
     end
 end
